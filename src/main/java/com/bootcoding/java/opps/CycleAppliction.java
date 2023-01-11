3db923db954699f41a20f5676ea3b582f10fca1b
Nachiket <Sarafnachiket@gmail.com>
2023-01-11 17:42:38 +0530

    Java program 3

---
+++ src/main/java/com/bootcoding/java/opps/CycleAppliction.java
@@ -0,0 +1,27 @@
+package com.bootcoding.java.opps;
+
+public class CycleAppliction {
+    public static void main(String[] args) {
+        Cycle c = new Cycle();
+        Cycle c1 = Cycle.createcycle();
+        Cycle c2 = Cycle.createcycle( "gc", 9, "hero");
+        Cycle.printcycle(c1);
+        Cycle.printcycle(c2);
+        c1.brand = "HERO";
+        c1.type = "GearCycle";
+        c1.weight = 10;
+
+        GearCycle gc = new GearCycle();
+        gc.brand = "Herculys";
+        gc.type = "GearCycle";
+        gc.weight = 15;
+
+        MountainCycle mc = new MountainCycle();
+        mc.brand = "MountainCycle";
+        mc.type = "Mountain Cycle";
+        mc.weight = 8;
+
+
+
+    }
+}
