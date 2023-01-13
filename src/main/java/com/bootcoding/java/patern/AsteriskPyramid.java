fe506723a942d019e19e615601e30992031ad4c2
Nachiket <Sarafnachiket@gmail.com>
2023-01-14 01:30:15 +0530

    Pattern like a pyramid with an asterisk

---
+++ src/main/java/com/bootcoding/java/patern/AsteriskPyramid.java
@@ -0,0 +1,20 @@
+package com.bootcoding.java.patern;
+
+public class AsteriskPyramid {
+    public static void main(String[] args) {
+     printAsteriskPyramid();
+    }
+    private static void printAsteriskPyramid(){
+        int row = 4;
+        for (int i = 1 ; i <= row  ; i++){
+            for (int j = row-i ; j > 0 ; j--){
+                System.out.print(" ");
+            }
+            for (int j = 0 ; j < i ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+            }
+
+        }
+    }
