9ba7502f42c800c8ec89e7762900faa77078a8b7
Nachiket <Sarafnachiket@gmail.com>
2023-01-14 01:52:28 +0530

    table

---
+++ src/main/java/com/bootcoding/java/opps/inheritance/Table.java
@@ -0,0 +1,16 @@
+package com.bootcoding.java.opps.inheritance;
+
+public class Table {
+    int legs;
+    boolean tabletop;
+    String tabletype;
+    Material material;
+
+    public void move(){
+        System.out.println("Move the table to another location");
+    }
+    public void createTable(){
+        System.out.println("Create new table");
+    }
+}
+
