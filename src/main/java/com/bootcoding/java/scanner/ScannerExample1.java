f740e16410e97c4df1931d836ea909a416083c42
Nachiket <Sarafnachiket@gmail.com>
2023-01-15 00:19:20 +0530

    ScannerExample1

---
+++ src/main/java/com/bootcoding/java/scanner/ScannerExample1.java
@@ -0,0 +1,10 @@
+package com.bootcoding.java.scanner;
+
+public class ScannerExample1 {
+    public static void main(String[] args) {
+        System.out.println("enter");
+    }
+    public static boolean isEven(int value){
+        return value % 2 == 0;
+    }
+}
