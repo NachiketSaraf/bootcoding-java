306b05648edf1b15cde9898275e92967ef47b3a9
Nachiket <Sarafnachiket@gmail.com>
2023-01-20 00:26:03 +0530

    Fibonacci using for loop.

---
+++ src/main/java/com/bootcoding/java/Fibonacci.java
@@ -0,0 +1,16 @@
+package com.bootcoding.java;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int a = 0,b=1;
+        int c = 0;
+        System.out.println(a);
+        System.out.println(b);
+        for (int i = 2;i<=10;i++){
+            c=a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
+        }
+    }
+}
