16e6d855d20c3926450d9eac54092583077ecf5d
Nachiket <Sarafnachiket@gmail.com>
2023-01-20 13:41:48 +0530

    WAP to display Pascal's triangle.

---
+++ src/main/java/com/bootcoding/java/patern/PascalTriangle.java
@@ -0,0 +1,34 @@
+package com.bootcoding.java.patern;
+
+public class PascalTriangle {
+    public static void main(String[] args) {
+        printPascaleTriangle();
+    }
+
+    private static void printPascaleTriangle() {
+        int n =5;
+        int res = 0;
+        int count = 1;
+        for(int i=0; i<n; i++){
+            for(int j=n-i; j>=1; j--) {
+
+                System.out.print(" ");
+            }
+            for (int l=0; l<=i; l++){
+                res = res + 1;
+                if(l == 0 || l == i){
+                    count = 1;
+                }else{
+                    count=count*(i-l+1)/l; // 1. Parenthesis, 2.substraction, 3. Addition, 4. Multiplication, 5.Division
+                }
+
+                System.out.print(count + " ");
+            }
+
+
+            System.out.println();
+
+        }
+    }
+}
+
