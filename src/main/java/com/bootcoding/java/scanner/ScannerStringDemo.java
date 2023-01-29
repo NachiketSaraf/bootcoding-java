2b0d97b0b26c9beb831e8708d38553ba589e78ae
Nachiket <Sarafnachiket@gmail.com>
2023-01-29 22:26:41 +0530

    scamer demo

--- src/main/java/com/bootcoding/java/scanner/ScannerStringDemo.java
+++ src/main/java/com/bootcoding/java/scanner/ScannerStringDemo.java
@@ -1,26 +1,26 @@
-package com.bootcoding.java.scanner;
-
-import javax.swing.plaf.synth.SynthOptionPaneUI;
-import java.util.Scanner;
-
-public class ScannerStringDemo {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter word");
-        String word = sc.next();
-        char output = findDuplicateChar(word);
-        System.out.println("Duplicate charcter in " + word + "is '" + output + "'");
-    }
-
-    private static char findDuplicateChar(String word) {
-        char [] chars =word.toCharArray();
-                 for (int i = 0;i<chars.length;i++){
-                     for (int j = 0; j<i;i++){
-
-                     }
-                 }
-        return chars[];
-    }
-}
-//wap take input from scanner
-
+//package com.bootcoding.java.scanner;
+//
+//import javax.swing.plaf.synth.SynthOptionPaneUI;
+//import java.util.Scanner;
+//
+//public class ScannerStringDemo {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter word");
+//        String word = sc.next();
+//        char output = findDuplicateChar(word);
+//        System.out.println("Duplicate charcter in " + word + "is '" + output + "'");
+//    }
+//
+//    private static char findDuplicateChar(String word) {
+//        char [] chars =word.toCharArray();
+//                 for (int i = 0;i<chars.length;i++){
+//                     for (int j = 0; j<i;i++){
+//
+//                     }
+//                 }
+//        return chars[];
+//    }
+//}
+////wap take input from scanner
+//
