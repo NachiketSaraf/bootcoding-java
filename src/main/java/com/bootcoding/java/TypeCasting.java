7c50734257b2ed630678c7036758b8e7818625d7
Nachiket <Sarafnachiket@gmail.com>
2023-02-06 23:39:23 +0530

    Type casting example

---
+++ src/main/java/com/bootcoding/java/TypeCasting.java
@@ -0,0 +1,17 @@
+package com.bootcoding.java;
+
+public class TypeCasting {
+
+
+
+
+    public static void main(String[] args){
+            float n = (float) 1.2;
+            float f = 1.2f;
+            long l = 214748364892l;
+            System.out.println(n);
+            System.out.println(f);
+            System.out.println(l);
+
+        }
+}
