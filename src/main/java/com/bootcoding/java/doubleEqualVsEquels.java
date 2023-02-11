6f2722da326cda309f68f2dc3396846dd8aef39d
Nachiket <Sarafnachiket@gmail.com>
2023-02-12 02:24:59 +0530

    Difference Between == and .equals() with String pool explanation

--- src/main/java/com/bootcoding/java/doubleEqualVsEquels.java
+++ src/main/java/com/bootcoding/java/doubleEqualVsEquels.java
@@ -8,16 +8,22 @@ public class doubleEqualVsEquels {
         String e = new String("java");
         String d = new String("java").intern();
 
-        System.out.println(a==b);//it will check for reference is same or not
-        System.out.println(a.equals(b));//it will check for the content in object
 
+        System.out.print("a==b ");
+        System.out.println(a==b);//it will check for reference is same or not
+        System.out.println("a.equals(b) " + a.equals(b));//it will check for the content in object
+
+        System.out.print("a==c ");
         System.out.println(a==c);
         // here the content is same but the reference and object is different so it will return false
         // because of the == operator
-        System.out.println(a.equals(c));
+        System.out.println("a.equals(c) " +a.equals(c));
         //here we use .equels() which check for the actual value inside the object
         //thats why it is returning true
 
+        System.out.print("c==e ");
+        System.out.println(c==e);
+        System.out.println("c.equals(e) " + c.equals(e));
 
     }
 }
