c79b8016cdfb15189a572819cda9580df5399c37
Nachiket <Sarafnachiket@gmail.com>
2023-02-14 23:58:30 +0530

    Asterisk Right Angle Triangle

--- src/main/java/com/bootcoding/java/patern/AsteriskRightAngleTriangle.java
+++ src/main/java/com/bootcoding/java/patern/AsteriskRightAngleTriangle.java
@@ -2,12 +2,12 @@ package com.bootcoding.java.patern;
 
 public class AsteriskRightAngleTriangle {
     public static void main(String[] args) {
-        printRightAngleTranngle();
+        printRightAngleTranngle(1);
     }
-    private static void printRightAngleTranngle(){
-        for (int i =0 ; i <= 4 ; i++ ){
+    private static void printRightAngleTranngle(int n){
+        for (int i =0 ; i <= n ; i++ ){
             for (int j = 0 ; j < i ; j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
