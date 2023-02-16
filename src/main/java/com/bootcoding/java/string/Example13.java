e7989d5fe9838e5a6917ba27d7bcd64fb1089967
Nachiket <Sarafnachiket@gmail.com>
2023-02-16 19:39:20 +0530

    String Handling .compareTo(String);

---
+++ src/main/java/com/bootcoding/java/string/Example13.java
@@ -0,0 +1,12 @@
+package com.bootcoding.java.string;
+
+public class Example13 {
+    public static void main(String[] args) {
+        String str ="ram";
+        System.out.println(str.compareTo("ram"));
+        System.out.println(str.compareTo("raj"));
+        System.out.println(str.compareTo("Ram"));
+        System.out.println(str.compareTo(" Ram"));
+        System.out.println(str.compareTo("satish"));
+    }
+}
