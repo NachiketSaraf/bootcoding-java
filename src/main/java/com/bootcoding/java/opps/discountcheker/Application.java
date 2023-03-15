6c61e08c61c66793349160dc7474edb8a07c33b3
Nachiket <Sarafnachiket@gmail.com>
2023-03-15 23:56:41 +0530

    Application class to create customer and apply for discount Object and class practice

---
+++ src/main/java/com/bootcoding/java/opps/discountcheker/Application.java
@@ -0,0 +1,9 @@
+package com.bootcoding.java.opps.discountcheker;
+
+public class Application {
+    public static void main(String[] args) {
+        Customer ram = new Customer();
+        ram.name = "";
+    }
+}
+
