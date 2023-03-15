a5b60e0d275533140ac189d04c58fa74d18b48c5
Nachiket <Sarafnachiket@gmail.com>
2023-03-16 00:07:47 +0530

    command line argument send argument in command line and printing them

---
+++ src/main/java/com/bootcoding/java/comandLineArgument/LowerToUpperCase.java
@@ -0,0 +1,9 @@
+package com.bootcoding.java.comandLineArgument;
+
+public class LowerToUpperCase {
+    public static void main(String[] args) {
+        for (int i = 0;i< args.length;i++){
+            System.out.println(args[i].toUpperCase());
+        }
+    }
+}
