1f43297e4f8d523d6c2793a062b0f633f1898c09
Nachiket <Sarafnachiket@gmail.com>
2023-03-15 23:54:13 +0530

    Customer class Object and class practice

---
+++ src/main/java/com/bootcoding/java/opps/discountcheker/Customer.java
@@ -0,0 +1,11 @@
+package com.bootcoding.java.opps.discountcheker;
+
+public class Customer {
+    String type;
+    String name;
+
+    public void setCustomer(String type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+}
