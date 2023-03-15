7ad596bc0dbab18a24596f9c4de1aa7ca81f023a
Nachiket <Sarafnachiket@gmail.com>
2023-03-15 23:59:47 +0530

    created customer object using getter setter

---
+++ src/main/java/com/bootcoding/java/opps/classes/getset/MyEmployee.java
@@ -0,0 +1,13 @@
+package com.bootcoding.java.opps.classes.getset;
+
+public class MyEmployee {
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        employee.setName("dummy");
+        employee.setAge(19);
+        employee.setPhone(793017794);
+        System.out.println(employee);
+
+    }
+}
+
