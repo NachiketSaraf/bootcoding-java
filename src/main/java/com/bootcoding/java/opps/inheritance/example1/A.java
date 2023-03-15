0610cbba6f63e375ca6fe1692fa0633052577ff5
Nachiket <Sarafnachiket@gmail.com>
2023-03-16 00:05:01 +0530

    Inheritance example with same variable name in both class

---
+++ src/main/java/com/bootcoding/java/opps/inheritance/example1/A.java
@@ -0,0 +1,15 @@
+package com.bootcoding.java.opps.inheritance.example1;
+
+class A {
+    protected int a;
+
+    void setA(int n1) {
+        a = n1;
+
+
+    }
+
+    void showA() {
+        System.out.println("A =  " + a);
+    }
+}
