f9af9a962b7e968c739e23e04c8d1c3057bd51d4
Nachiket <Sarafnachiket@gmail.com>
2023-03-17 19:14:17 +0530

    Application class to create customer and apply for discount

--- src/main/java/com/bootcoding/java/opps/discountcheker/Application.java
+++ src/main/java/com/bootcoding/java/opps/discountcheker/Application.java
@@ -3,7 +3,11 @@ package com.bootcoding.java.opps.discountcheker;
 public class Application {
     public static void main(String[] args) {
         Customer ram = new Customer();
-        ram.name = "";
+        ram.name = "ram";
+        ram.type ="Regular";
+        Discount ramDiscount = new Discount();
+        int discount = ramDiscount.giveDiscount(ram);
+        System.out.println(ram.name+" is a "+ram.type+" customer and got a discount of "+discount);
     }
 }
 
