6015597f528f105d70f7ac8897c2dac5fd0aaeb1
Nachiket <Sarafnachiket@gmail.com>
2023-03-20 19:34:09 +0530

    check if no is palendrom accept from CLI

---
+++ src/main/java/com/bootcoding/java/comandLineArgument/Palendrom.java
@@ -0,0 +1,21 @@
+package com.bootcoding.java.comandLineArgument;
+
+public class Palendrom {
+    public static void main(String[] args) {
+        checkPalendrom(args);
+    }
+    public static void checkPalendrom(String[] num){
+        for (int i = 0; i< num.length;i++){
+            int n = Integer.parseInt(num[i]);
+            int temp = n;
+            int rem=0;
+            while (n!=0){
+                rem = rem*10 + n%10;
+                n = n/10;
+            }
+            if(temp == rem){
+                System.out.println(temp+" is Palendrom");
+            }
+        }
+    }
+}
