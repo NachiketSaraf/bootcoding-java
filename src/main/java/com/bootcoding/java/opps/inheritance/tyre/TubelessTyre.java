08cf8a2d9610054ad15acb767b9f787273e0e04b
Nachiket <Sarafnachiket@gmail.com>
2023-03-27 00:36:52 +0530

    tubeless tyre class Inherited form Tyre class

--- src/main/java/com/bootcoding/java/opps/inheritance/tyre/TubelessTyre.java
+++ src/main/java/com/bootcoding/java/opps/inheritance/tyre/TubelessTyre.java
@@ -3,12 +3,11 @@ package com.bootcoding.java.opps.inheritance.tyre;
 public class TubelessTyre extends Tyre{
     @Override
     public String toString() {
-        return "TubelessTyre{" +
-                "company='" + company + '\'' +
-                ", material='" + material + '\'' +
-                ", type='" + type + '\'' +
-                ", compatable='" + compatable + '\'' +
-                ", diameter=" + diameter +
-                '}';
+        return "TubelessTyre > " +
+                "company= '" + company + '\'' +
+                ", material= '" + material + '\'' +
+                ", type= '" + type + '\'' +
+                ", compatable= '" + compatable + '\'' +
+                ", diameter=" + diameter;
     }
 }
