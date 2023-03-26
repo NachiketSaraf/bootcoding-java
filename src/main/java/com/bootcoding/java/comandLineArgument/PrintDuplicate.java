4e93bd9b9acaa4e9bf682af1fbfcac710ad19c3d
Nachiket <Sarafnachiket@gmail.com>
2023-03-26 23:21:12 +0530

    do task on array data as per the user give in first element in CLI

--- src/main/java/com/bootcoding/java/comandLineArgument/PrintDuplicate.java
+++ src/main/java/com/bootcoding/java/comandLineArgument/PrintDuplicate.java
@@ -5,11 +5,74 @@ import java.util.Map;
 
 public class PrintDuplicate {
     public static void main(String[] args) {
+
         if (args[0].equals("-f")){
             printFrequncyAndDuplicate(args);
         }
+        else if (args[0].equals("-min")){
+            printNumberWithMinFrequncy(args);
+        }else if (args[0].equals("-max")){
+            printNumberWithMaxFrequncy(args);
+        }
         else printDuplicate(args);
     }
+
+    private static void printNumberWithMaxFrequncy(String[] arr) {
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 1;i< arr.length;i++){
+            int n = Integer.parseInt(arr[i]);
+            if(map.containsKey(n)){
+                int value = map.get(n);
+                map.put(n,value+1);
+            }
+            else {
+                map.put(n,1);
+            }
+        }
+        int max=Integer.MIN_VALUE;
+        for (Integer key : map.keySet()) {
+            int value = map.get(key);
+            if (max<value){
+                max=value;
+            }
+        }
+        for (Integer key : map.keySet()) {
+            int value = map.get(key);
+            if (max==value){
+                System.out.println(key+" has minimum frequncy");
+            }
+        }
+    }
+
+    private static void printNumberWithMinFrequncy(String[] arr) {
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 1;i< arr.length;i++){
+            int n = Integer.parseInt(arr[i]);
+            if(map.containsKey(n)){
+                int value = map.get(n);
+                map.put(n,value+1);
+            }
+            else {
+                map.put(n,1);
+            }
+        }
+        int min=Integer.MAX_VALUE;
+        for (Integer key : map.keySet()) {
+            int value = map.get(key);
+            if (min>value){
+                min=value;
+            }
+        }
+        for (Integer key : map.keySet()) {
+            int value = map.get(key);
+            if (min==value){
+                System.out.println(key+" has minimum frequncy");
+            }
+        }
+    }
+
     public static void printDuplicate(String[] arr){
         Map<Integer,Integer> map = new HashMap<>();
 
