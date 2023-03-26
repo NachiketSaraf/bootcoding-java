cc5c04a0b3686a3dbd172512b1132f339f626c2e
Nachiket <Sarafnachiket@gmail.com>
2023-03-26 23:27:41 +0530

    First example of classes employee

---
+++ src/main/java/com/bootcoding/java/opps/classes/example1/Employee.java
@@ -0,0 +1,13 @@
+package com.bootcoding.java.opps.classes.example1;
+
+public class Employee {
+    public static void main(String args[]){
+        Employee1 e1 = new Employee1();
+
+        e1.setEmployee("me",21,1000);
+        e1.showEmployee();
+        e1.calculate();
+    }
+
+}
+
