c433eaa98723f6d1c3a5191d733b54f2495ed07e
Nachiket <Sarafnachiket@gmail.com>
2023-03-26 23:31:16 +0530

    application class for tyre

---
+++ src/main/java/com/bootcoding/java/opps/inheritance/tyre/Application.java
@@ -0,0 +1,24 @@
+package com.bootcoding.java.opps.inheritance.tyre;
+
+public class Application {
+    public static void main(String[] args) {
+        TubelessTyre tl = new TubelessTyre();
+        tl.company = "MRF";
+        tl.material = "Rubber";
+        tl.type = "TubeLess";
+        tl.compatable = "Car";
+        tl.diameter = 17;
+        System.out.println(tl);
+
+        System.out.println();
+
+        TubeTyre tt = new TubeTyre();
+        tt.company="CEAT";
+        tt.material="Rubber";
+        tt.type = "Tuber";
+        tt.compatable ="Mountain bike";
+        tt.diameter = 20;
+        System.out.println(tt);
+    }
+
+}
