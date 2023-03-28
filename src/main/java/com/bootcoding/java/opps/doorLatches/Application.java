33a8f2c6f46ed7284d01c9b3c4d1e48da9ade77d
Nachiket <Sarafnachiket@gmail.com>
2023-03-28 23:54:38 +0530

    many to one has a relationship example

--- src/main/java/com/bootcoding/java/opps/doorLatches/Application.java
+++ src/main/java/com/bootcoding/java/opps/doorLatches/Application.java
@@ -4,5 +4,9 @@ public class Application {
     public static void main(String[] args) {
         Latches[] latches = new Latches[4];
 
+        for (int i = 0;i<4;i++){
+            latches[i] = new Latches();
+        }
+
     }
 }
